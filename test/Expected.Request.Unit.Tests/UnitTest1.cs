d9f5af81cbd8bf7cfd685a5742f08286eb0c03c0
Darrius Wright <darriuswrightgd@gmail.com>
2017-09-25 22:49:05 -0600

    Adding unit tests and separating travis stages

---
+++ test/Expected.Request.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Expected.Request.Unit.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
